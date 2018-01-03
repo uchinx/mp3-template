f5d01172f83ee710fa82a4e364ad8d6afe47119a
Tèn <uchinkaa@gmail.com>
2018-01-03 20:13:06 +0700

    update album code

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ var app = {
 var playlist = {
 			type: 'album',
 			code: [
-				'kmxHTLGGuZWuRTGTvHkHtLpLBdZQFgz',
+				'LHcHyLnRQRmZCbRymtFHkntZpZliuHihk',
 				'ZGcGtkmapAZpRNQymyFnLntZpLbRadJaB',
 				'ZGcGtLHsQzcvNmRyHtFHLHykQZDRNBVZC',
 				'kGcnTLHsWpGXJzvymybGLnyZQLbiadFEN'
